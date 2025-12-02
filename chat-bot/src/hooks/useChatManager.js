aab9ce5256eeb8b51b495960d6b7024002fee41a
since122522 <salindongmike@gmail.com>
2025-12-02 15:25:00 +0800

    update

--- chat-bot/src/hooks/useChatManager.js
+++ chat-bot/src/hooks/useChatManager.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useCallback } from 'react';
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = '';
 
 const useChatManager = (user) => {
     // Stores the list of conversations (id and title)
